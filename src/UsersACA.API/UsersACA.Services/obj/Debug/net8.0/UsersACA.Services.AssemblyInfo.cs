17d29cc64ea7271411065db9cd08dbc92acd4e04
christian-cell <cristohp74@gmail.com>
2025-03-05 20:59:09 +0100

    commit

--- src/UsersACA.API/UsersACA.Services/obj/Debug/net8.0/UsersACA.Services.AssemblyInfo.cs
+++ src/UsersACA.API/UsersACA.Services/obj/Debug/net8.0/UsersACA.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersACA.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+148e190b8bdaeaf88dd5fa34f8d9391df9a9c392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd820777f93eea534cc127180a8909ae45439a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersACA.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersACA.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
