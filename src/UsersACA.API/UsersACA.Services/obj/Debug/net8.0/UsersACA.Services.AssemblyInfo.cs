8a51102a417a45cffc09736d13a13dfafba396eb
christian-cell <cristohp74@gmail.com>
2025-03-11 16:38:21 +0100

    new message of the UserService

--- src/UsersACA.API/UsersACA.Services/obj/Debug/net8.0/UsersACA.Services.AssemblyInfo.cs
+++ src/UsersACA.API/UsersACA.Services/obj/Debug/net8.0/UsersACA.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersACA.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbd14b00d5086637915d307cb4b31db9eedb2a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee3888f77e0dce9aaf860f9e87124d4c9c57023")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersACA.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersACA.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
